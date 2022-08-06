0755828c13abe15a73b9f02e5a57ecacf20a7dac
林鹏涛 <326661445@qq.com>
2022-08-06 15:59:33 +0800

    彻底完成左边导航栏

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -3,17 +3,35 @@ import "./style.scss"
 
 const Layout = () => {
     const [isShowInsp,setIsShowInsp]=useState(false)
-    function handleShowInspiration(e){
+    const [isShowMaterial,setIsShowMaterial]=useState(false)
+    function handleShowInspiration(){
         let ele =document.querySelector(".Layout-Items-li-many")
+        let Iele =document.querySelector(".inspiration-many-youjiantou")
         if(!isShowInsp){
-            e.target.lastChild.className="iconfont icon-shanglajiantou inspiration-many-youjiantou"
+            Iele.className="iconfont icon-shanglajiantou inspiration-many-youjiantou"
             ele.style.display="block"
             setIsShowInsp(isShowInsp=>!isShowInsp)
         }
         else{
-            e.target.lastChild.className="iconfont icon-youjiantou inspiration-many-youjiantou"
+            Iele.className="iconfont icon-youjiantou inspiration-many-youjiantou"
             ele.style.display="none"
             setIsShowInsp(isShowInsp=>!isShowInsp)
+
+        }
+    }
+    function handleShowMaterial(){
+        let ele =document.querySelector(".ShowMaterial")
+        let Iele =document.querySelector(".Material-many-youjiantou")
+        if(!isShowMaterial){
+            Iele.className="iconfont icon-shanglajiantou Material-many-youjiantou"
+            ele.style.display="block"
+            setIsShowMaterial(isShowMaterial=>!isShowMaterial)
+        }
+        else{
+            Iele.className="iconfont icon-youjiantou Material-many-youjiantou"
+            ele.style.display="none"
+            setIsShowMaterial(isShowMaterial=>!isShowMaterial)
+
         }
     }
     return (
@@ -23,52 +41,106 @@ const Layout = () => {
             </div>
             <ul className="Layout-Items">
                 <li >
-                    <i className="iconfont icon-sousuo"></i> <span>搜索</span>
+                    <a href="javascript:;"><i className="iconfont icon-sousuo"></i>
+                        <span>搜索</span>
+                    </a>
                 </li>
                 <li >
-                    <i className="iconfont icon-html"></i> <span>框架类库</span>
+                    <a href="javascript:;"><i className="iconfont icon-html"></i>
+                        <span>框架类库</span>
+                    </a>
                 </li>
                 <li >
-                    <i className="iconfont icon-github5"></i> <span>项目托管</span>
+                    <a href="javascript:;"><i className="iconfont icon-github5"></i>
+                        <span>项目托管</span>
+                    </a>
+
                 </li>
                 <li >
-                    <i className="iconfont icon-shequ"></i> <span>开发社区</span>
+                    <a href="javascript:;"> <i className="iconfont icon-shequ"></i>
+                        <span>开发社区</span>
+                    </a>
+
                 </li>
                 <li >
-                    <i className="iconfont icon-cms-c"></i> <span>CMS程序</span>
+                    <a href="javascript:;"><i className="iconfont icon-cms-c"></i>
+                        <span>CMS程序</span>
+                    </a>
+
                 </li>
                 <li >
-                    <i className="iconfont icon-file-powerpoint"> </i> <span>字体图标</span>
+                    <a href="javascript:;"><i className="iconfont icon-file-powerpoint"> </i>
+                        <span>字体图标</span>
+                    </a>
+
                 </li>
                 <li >
-                    <i className="iconfont icon-HTML-fill"></i> <span>IDE工具</span>
+                    <a href="javascript:;"><i className="iconfont icon-HTML-fill"></i>
+                        <span>IDE工具</span>
+                    </a>
+
                 </li>
                 <li >
-                    <i className="iconfont icon-_kecheng"></i> <span>在线课堂</span>
+                    <a href="javascript:;"> <i className="iconfont icon-_kecheng"></i>
+                        <span>在线课堂</span>
+                    </a>
+
                 </li>
-                <li onClick={handleShowInspiration}>
-                    <i className="iconfont icon-lightbulb"></i> <span>灵感采集</span> <i className="iconfont icon-youjiantou inspiration-many-youjiantou"></i>
+                <li onClick={handleShowInspiration} >
+                    <a href="javascript:;">
+                        <i className="iconfont icon-lightbulb"></i>
+                        <span>灵感采集</span>
+                        <i className="iconfont icon-youjiantou inspiration-many-youjiantou"></i>
+                    </a>
+
                 </li>
                 <li className="Layout-Items-li-many">
                     <ul className="inspiration-many">
-                        <li>发现产品</li>
-                        <li>界面灵感</li>
-                        <li>网页灵感</li>
+                        <li><a href="javascript:;">发现产品</a></li>
+                        <li><a href="javascript:;">界面灵感</a></li>
+                        <li><a href="javascript:;">网页灵感</a></li>
                     </ul>
                 </li>
                 <li >
-                    <i className="iconfont icon-cloud-full"></i> <span>云服务</span>
+                    <a href="javascript:;"><i className="iconfont icon-cloud-full"></i>
+                        <span>云服务</span>
+                    </a>
+
+                </li>
+                <li onClick={handleShowMaterial} >
+                    <a href="javascript:;">
+                        <i className="iconfont icon-briefcase-full"></i>
+                        <span>素材资源</span>
+                        <i className="iconfont icon-youjiantou Material-many-youjiantou"/>
+                    </a>
+                </li>
+                <li className="ShowMaterial" >
+                    <ul className="ShowMaterial-ul">
+                        <li><a href="javascript:;"> LOGO设计</a></li>
+                        <li><a href="javascript:;"> Mockup</a></li>
+                        <li><a href="javascript:;"> PPT资源</a></li>
+                        <li><a href="javascript:;"> Sketch资源</a></li>
+                        <li><a href="javascript:;">UI资源 </a></li>
+                        <li><a href="javascript:;"> 图标素材</a></li>
+                        <li><a href="javascript:;">字体资源 </a></li>
+                        <li><a href="javascript:;"> 平面素材</a></li>
+                        <li><a href="javascript:;"> 摄影图库</a></li>
+                    </ul>
                 </li>
                 <li >
-                    <i className="iconfont icon-briefcase-full"></i>
-                    <span>素材资源</span>
-                </li>
-                <li >
-                    <i className="iconfont icon-youqinglianjie"></i> <span>友情链接</span>
+                    <a href="javascrpit:;">
+                        <i className="iconfont icon-youqinglianjie"></i>
+                        <span>友情链接</span>
+                    </a>
+
                 </li>
             </ul>
             <div className="Layout-Record">
-                <i className="iconfont icon-ceshishenqing"></i> <span>申请收录</span>
+                <a href="javascrpit:;">
+                    <i className="iconfont icon-ceshishenqing"></i>
+                    <span>申请收录</span>
+                </a>
+
             </div>
         </div>
     );
