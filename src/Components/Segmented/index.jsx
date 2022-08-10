c7e95ef8cee1fb9ea569551108e27641987bfff5
admin <2915017234@qq.com>
2022-08-10 10:59:48 +0800

    Home-Category

--- src/Components/Segmented/index.jsx
+++ src/Components/Segmented/index.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 import "./style.scss"
 
-const Segmented = () =>{
-    return(
-        <div id="app">
-	            <ul>
-		        <li>热门网址</li>
-		        <li>大家喜欢</li>
-		        <li>最新网址</li>
-		        <div class="focus-el el--1"></div>
-		        <div class="focus-el el--2"></div>
-	        </ul>
-        </div>
-    )
+const Segmented = () => {
+	
+	return (
+		<div id="app">
+			<ul>
+				<li>热门网址</li>
+				<li>大家喜欢</li>
+				<li>最新网址</li>
+				<div class="focus-el el--1"></div>
+				<div class="focus-el el--2"></div>
+			</ul>
+		</div>
+	)
 }
 export default Segmented;
--- src/pages/FirstPage/index.jsx
+++ src/pages/FirstPage/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./style.scss"
 import Layout from "../../Components/Layout/index.jsx";
 import TopNav from "../../Components/TopNav/index.jsx";
+import Segmented from '../../Components/Segmented/index.jsx';
 
 const FirstPage = () => {
     return (
         <div>
             <Layout/>
             <TopNav/>
+            <Segmented/>
 
         </div>
     );
