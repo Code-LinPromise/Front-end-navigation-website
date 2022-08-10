74064d05d677a00dee0c7f0c3d452bc8c7e5607b
admin <2915017234@qq.com>
2022-08-10 15:07:08 +0800

    Segmented组件编写

--- src/Components/Segmented/index.jsx
+++ src/Components/Segmented/index.jsx
@@ -1,19 +1,80 @@
-import React from "react";
+import {useEffect, useRef, useState} from 'react';
 
-import "./style.scss"
+import './style.scss'
 
+const onMouseOver = (setCurrentIndex) => {
+    return (e) => {
+        const index = e.target.getAttribute('index')
+        if (index !== undefined && index !== null) {
+            setCurrentIndex(index)
+        }
+    }
+}
+const onMouseLeave = (setCurrentIndex, clickIndex) => {
+    return () => {
+        setCurrentIndex(clickIndex)
+    }
+}
+const onClick = (setClickIndex) => {
+    return (e) => {
+        const index = e.target.getAttribute('index')
+        if (index !== undefined && index !== null) {
+            setClickIndex(index)
+        }
+    }
+}
+const obj = [
+    '热门网址',
+    '大家喜欢',
+    '最新网址'
+]
 const Segmented = () => {
-	
-	return (
-		<div id="app">
-			<ul>
-				<li>热门网址</li>
-				<li>大家喜欢</li>
-				<li>最新网址</li>
-				<div class="focus-el el--1"></div>
-				<div class="focus-el el--2"></div>
-			</ul>
-		</div>
-	)
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [refresh, setRefresh] = useState(false)
+    const [clickIndex, setClickIndex] = useState(0)
+    const el = useRef(null)
+    const interval = useRef(null)
+    const left = useRef(0)
+    useEffect(() => {
+        let end = currentIndex * 33;
+        if (interval.current) {
+            clearInterval(interval.current)
+            interval.current = null
+        }
+        const eachStep = Math.abs(currentIndex - clickIndex) <= 0 ? 3 : Math.abs(currentIndex - clickIndex) * 2
+        interval.current = setInterval(() => {
+            if (left.current < end) {
+                if (left.current + eachStep > end) {
+                    left.current = end
+                } else {
+                    left.current += eachStep
+                }
+            } else if (left.current > end) {
+                if (left.current - eachStep < end) {
+                    left.current = end
+                } else {
+                    left.current -= eachStep
+                }
+            } else {
+                clearInterval(interval.current)
+                interval.current = null
+            }
+            setRefresh((prev) => !prev)
+        }, 12)
+    }, [currentIndex, clickIndex])
+    return (
+        <div className={'segment'}>
+            <ul onMouseOver={onMouseOver(setCurrentIndex)} onMouseLeave={onMouseLeave(setCurrentIndex, clickIndex)}
+                onClick={onClick(setClickIndex)}>
+                {obj.map((item, index) => {
+                    return (
+                        <li key={index} index={index}
+                            style={index == currentIndex ? {color: 'white'} : {color: 'rgb(136,136,136)'}}>{item}</li>
+                    )
+                })}
+                <div className="active" ref={el} style={{left: `${left.current}%`}}></div>
+            </ul>
+        </div>
+    )
 }
 export default Segmented;
