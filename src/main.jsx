96b56040ba63a44b4c3c4ab9a755aefcf2de4f09
JayZyyyy <1538415595@qq.com>
2022-08-17 12:08:06 +0800

    Item组件封装

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import "./Shared/style.scss"
-import "../src/assets/Iconfont/iconfont.css"
-import "../src/assets/IconTeam/iconfont.css"
+// import "../src/assets/Iconfont/iconfont.css"
+// import "../src/assets/IconTeam/iconfont.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/FirstPage/index.jsx
+++ src/pages/FirstPage/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import "./style.scss"
 import Layout from "../../Components/Layout/index.jsx";
 import TopNav from "../../Components/TopNav/index.jsx";
+import LinkItem from "../../Components/LinkItem/index.jsx";
 
 const FirstPage = () => {
 
@@ -9,7 +10,7 @@ const FirstPage = () => {
         <div className="FirstPage">
             <Layout/>
             <TopNav/>
-
+            <LinkItem itemName={"Github"} itemContent={"123456"} />
 
         </div>
     );
